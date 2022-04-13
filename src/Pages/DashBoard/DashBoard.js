afb77932bb90f694e14ebaf50fd2155c925d39b2
smehari57 <starmehari5@gmail.com>
2022-04-12 22:34:36 -0500

    waa-labs

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -1,42 +1,39 @@
- import React, { useState } from "react";
- import Post from "../../Components/Post";
- import Posts from "../../Containers/Posts";
- import "./DashBoard.css";
+import React, { useState } from "react";
+import Post from "../../Components/Post";
+import Posts from "../../Containers/Posts";
+import "./DashBoard.css";
 
 
 const DashBoard = () => {
-   const [title, setTitle] = useState("");
-   const [posts, setPosts] = useState(
-      [
-          {id:111, title:'Happiness', author:'John'},
-          {id:112, title:'MIU', author:'Dean'},
-          {id:113, title:'EnjoyLife', author:'Jasmine'}
-      ]);
-      const titleHandler = (title) => {
-          posts[0].title = title;
-          setPosts([...posts]);
-      }
+    const [title, setTitle] = useState("");
+    const [posts, setPosts] = useState(
+        [
+            { id: 111, title: 'Happiness', author: 'John' },
+            { id: 112, title: 'MIU', author: 'Dean' },
+            { id: 113, title: 'EnjoyLife', author: 'Jasmine' }
+        ]);
+    const titleHandler = (title) => {
+        posts[0].title = title;
+        setPosts([...posts]);
+    }
 
- return (
-    <div>
-    <Posts posts={posts}/>
-      <fieldset className="form">
-          <input
-              type="text"
-              id="newTitle"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Enter new title">
-          </input>
-          <br/><br/>
-          <button onClick={() => titleHandler(title)}>Change Title</button>
-      </fieldset>
-  </div>
-            
-    
-        
-        
-        
+    return (
+        <div>
+            <Posts posts={posts} />
+            <fieldset className="form">
+                <input
+                    type="text"
+                    id="newTitle"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Enter new title">
+                </input>
+                <br /><br />
+                <button onClick={() => titleHandler(title)}>Change Title</button>
+            </fieldset>
+        </div>
+
+
     );
 }
 export default DashBoard;
